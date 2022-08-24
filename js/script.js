0ac1354c3fc680120fbc16d8cfab08c8fea01f1f
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-08-24 16:58:18 +0200

    calcolo biglietto

--- js/script.js
+++ js/script.js
@@ -9,10 +9,23 @@ const calculateButton = document.getElementById("calculate");
 
 calculateButton.addEventListener("click",
     function () {
+        const distanceTrip = distanceInput.value;
         const ageGroup = ageInput.value;
         console.log(document.getElementById("name").value);
         console.log(document.getElementById("distance").value);
         console.log(document.getElementById("age").value);
-
+        if (ageGroup === "Maggiorenne") {
+            let prezzoBiglietto = distanceTrip * prezzoKm;
+            let risposta = `${prezzoBiglietto}€`;
+            document.getElementById("ticket-price").innerHTML = risposta;
+        } else if (ageGroup === "Minorenne") {
+            let prezzoBiglietto = distanceTrip * prezzoKm * (1 - scontoMinorenni);
+            let risposta = `${prezzoBiglietto}€`;
+            document.getElementById("ticket-price").innerHTML = risposta;
+        } else if (ageGroup === "Over-65") {
+            let prezzoBiglietto = distanceTrip * prezzoKm * (1 - scontoOver65);
+            let risposta = `${prezzoBiglietto}€`;
+            document.getElementById("ticket-price").innerHTML = risposta;
+        }
     }
 )
